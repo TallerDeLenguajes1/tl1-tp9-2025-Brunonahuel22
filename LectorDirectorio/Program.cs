dedcd8abd897c3007698fffacf1002c5f451a0cd
Bruno Cordoba <brunonahuel87@gmail.com>
2025-06-25 17:26:35 -0300

    termine lector tag

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.IO;
+
+
+string patchUsuario;
+/*
+bool respuesta;
+
+do
+{
+    Console.Write("Ingrese un directorio valido: ");
+    patchUsuario = Console.ReadLine();
+    respuesta = Directory.Exists(patchUsuario);
+    if (!respuesta)
+    {
+        Console.WriteLine("El directorio no existe");
+    }
+
+
+
+
+
+} while (!respuesta);
+
+*/
+
+patchUsuario = @"c:\Nueva carpeta";
+
+//Todas las carpetas que se encuentran en ese path
+
+string[] arregloDirectorios = Directory.GetDirectories(patchUsuario);
+
+foreach (var carpetas in arregloDirectorios)
+{
+
+    Console.WriteLine(Path.GetFileName(carpetas));
+}
+// lista para ir guadando cada linea 
+List<string> listaParaCVS = new List<string>();
+
+listaParaCVS.Add("Nombre,tamaño,fecha");
+
+//Todos los archivos que se encuentran directamente en esa carpeta
+string[] arregloDeArchivos = Directory.GetFiles(patchUsuario);
+
+foreach (var archivos in arregloDeArchivos)
+{
+    FileInfo archivo = new FileInfo(archivos);
+
+    double tamanioByte = archivo.Length;
+    double tamanioKB = tamanioByte / 1024.0;
+    string KBenString = tamanioKB.ToString("F2");
+
+
+    string fechaFormateadaParaCSV = archivo.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss");
+
+
+
+    listaParaCVS.Add($"{archivo.Name},{KBenString},{fechaFormateadaParaCSV}");
+}
+
+string nuevaRuta = Path.Combine(patchUsuario, "repostes.cvs");
+
+File.WriteAllLines(nuevaRuta, listaParaCVS);
+
+
+
---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text;
+
+string direccion = @"c:\Users\Estudiante\Desktop\tl1-tp9-2025-Brunonahuel22\LectorTagMP3";
+string archivo = @"prueba.mp3";
+
+string directorio = Path.Combine(direccion, archivo);
+
+using var archivomp3 = new FileStream(directorio, FileMode.Open);
+using var br = new BinaryReader(archivomp3);
+
+archivomp3.Seek(-128, SeekOrigin.End);
+
+byte[] arreglobyte = br.ReadBytes(128);
+
+string tag = Encoding.GetEncoding("latin1").GetString(arreglobyte, 0, 3).TrimEnd('\0');
+Console.WriteLine($"El tag es: {tag}");
+
+string titulo = Encoding.GetEncoding("latin1").GetString(arreglobyte, 3, 30).TrimEnd('\0');
+string artista = Encoding.GetEncoding("latin1").GetString(arreglobyte, 33, 30).TrimEnd('\0');
+string album = Encoding.GetEncoding("latin1").GetString(arreglobyte, 63, 30).TrimEnd('\0');
+string anio = Encoding.GetEncoding("latin1").GetString(arreglobyte, 93, 4).TrimEnd('\0');
+
+Console.WriteLine($"Título: {titulo}");
+Console.WriteLine($"Artista: {artista}");
+Console.WriteLine($"Álbum: {album}");
+Console.WriteLine($"Año: {anio}");
+
+
+
+
+
+
